c8b4a29a73d6599082f4e78c19bc64e88ba07a8a
Perry Smith <pedz@easesoftware.com>
2012-05-19 16:43:34 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Condor3::Application.config.secret_token = 'd550bc20f6a740cbf6033b89bc34e94694f3b1422b47994708917cb7701dae9fb1093161c91f7fccad358e27cbb52fed250822f2aa3d6d7690eb0d9fd0cd94e6'
