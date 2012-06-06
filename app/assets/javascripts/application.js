8ed23b0afc48785111de6810818a56f75416f15e
Perry Smith <pedz@easesoftware.com>
2012-06-06 07:47:13 -0500

    Lots of changes getting various UI elements working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jsviews/jsrender
+//= require jsviews/jquery.observable
+//= require jsviews/jquery.views
 //= require_tree .
--- app/assets/javascripts/upd_apar_def.js
+++ app/assets/javascripts/upd_apar_def.js
@@ -1,7 +1,33 @@
 $(document).ready(function () {
-    $.when( $.get('/assets/t1.html', null, null, 'html') ).done(function () {
-	console.log('done');
+    var upd_apar_defs_contextmenu = function (event) {
+	console.log(event);
+	var ul = $($(event.target).children()[0]);
+	var temp = {
+	    top: event.pageY,
+	    left: event.pageX
+	};
+	console.log(temp);
+	ul.css(temp);
+	ul.show();
+	return false;
+    };
+
+    $('.upd_apar_defs').on('contextmenu', 'td', upd_apar_defs_contextmenu);
+
+    $.when( $.get('/assets/t1.html', null, null, 'html') ).done(function (data, status, jqXHR) {
+	// data is a string.  $(data) turns into an array of HTML
+	// elements.  Often it is just one but if we get them ganged
+	// into one file, they will be more than one.
+	$(data).each(function () {
+	    // We assume the outside element is just a container.  I
+	    // use a <script> tag but it can be anything I suppose.
+	    // The id of the container becomes the template's name
+	    // while the contents becomes the template.
+	    $.templates(this.id, $(this).html());
+	    return this;
+	});
+	$('.upd_apar_defs tbody').html($.render.template1(json_elements.items));
     }).fail(function (a, b, c) {
-	console.log(a, b, c);
+	alert('Someone is really unhappy');
     });
 });
