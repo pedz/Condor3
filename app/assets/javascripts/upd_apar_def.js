2339dcbe99ac054d9e8f527ceaa953c6e5a565a9
Perry Smith <pedz@easesoftware.com>
2012-06-08 22:46:34 -0500

    Still a long ways to go but now have UI element bound within the table

--- app/assets/javascripts/upd_apar_def.js
+++ app/assets/javascripts/upd_apar_def.js
@@ -1,18 +1,40 @@
 $(document).ready(function () {
-    var upd_apar_defs_contextmenu = function (event) {
-	// 'this' will be the td which is the same as event.currentTarget
-	var children = $(this).children();
-	var ul = $(children[children.length - 1]);
-	var temp = {
+    var upd_apar_defs_click = function (event) {
+	// 'this' will be the tr which is the same as event.currentTarget
+	var ui = $($(this).find('.upd_apar_def_commands'));
+	var tbl;
+	var ui_width;
+	var tbl_right_edge;
+	var ui_right_edge;
+
+	// The width can change if the position is far right and
+	// causes the box to reform.  So, we absolutely position it at
+	// 0,0 and takes its width at that position.
+	ui.show();
+	ui.css({
+	    top: '0px',
+	    left: '0px'
+	});
+	ui_width = ui.outerWidth(true);
+
+	// Now position the ui at the mounse point.
+	ui.css({
 	    top: event.pageY,
 	    left: event.pageX
-	};
-	ul.css(temp);
-	ul.show();
+	});
+	
+	// Reposition it if the right edge is outside of the table
+	tbl = ui.parents('.upd_apar_defs');
+	tbl_right_edge = tbl.offset().left + tbl.innerWidth();
+	ui_right_edge = ui.offset().left + ui_width;
+	if (ui_right_edge > tbl_right_edge) {
+	    var new_left = (tbl_right_edge - ui_width);
+	    ui.css('left',  new_left + 'px');
+	}
 	return false;
     };
 
-    $('.upd_apar_defs').on('contextmenu', 'td', upd_apar_defs_contextmenu);
+    $('.upd_apar_defs').on('click', 'tbody tr', upd_apar_defs_click);
 
     $.when( $.get('/assets/t1.html', null, null, 'html') ).done(function (data, status, jqXHR) {
 	// data is a string.  $(data) turns into an array of HTML
