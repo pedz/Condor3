4f2446c9d7cce5238aa0822a268bbff569b6442d
Perry Smith <pedz@easesoftware.com>
2012-08-28 21:56:47 -0500

    Hopefully fix asset deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,14 +19,31 @@ set :deploy_via, :remote_cache
 set :git_enable_submodules, 1
 set :use_sudo, false
 
+# Don't need asset gems on production server
+set :bundle_without,  [:development, :test, :assets]
+
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
+  desc "No way to start the server"
   task :start do ; end
+  desc "No way to stop the server"
   task :stop do ; end
+  desc "Restart the server"
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+namespace :deploy do
+  namespace :assets do
+    desc 'Run the precompile task locally and rsync with shared'
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      %x{bundle exec rake assets:precompile}
+      %x{rsync --recursive --times --rsh=ssh --compress --human-readable --progress public/assets #{user}@#{host}:#{shared_path}}
+      %x{bundle exec rake assets:clean}
+    end
   end
 end
