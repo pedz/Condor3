39d895a7f5e36004b0d185ec5a3214d95f81b4b2
Perry Smith <pedz@easesoftware.com>
2012-09-02 19:12:44 -0500

    Trying to fix prefix for local set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Condor3::Application.routes.draw do
   constraints(:item => /[^\/]+/) do # item can have dots in it e.g. bos.mp64 6.1.7.3
     get '/swinfos/:item/:sort/:page' => 'swinfos#show', :as => "swinfo"
+    if Rails.application.config.relative_url_root
+      tmp = "#{Rails.application.config.relative_url_root}/swinfos"
+    else
+      tmp = "/swinfos"
+    end
     match '/swinfos/:item' => redirect { |params, req|
-      URI.escape("/swinfos/#{params[:item]}/#{SwinfosController::DEFAULT_SORT_ORDER}/1")
+        URI.escape("#{tmp}/#{params[:item]}/#{SwinfosController::DEFAULT_SORT_ORDER}/1")
     }
   end
   
