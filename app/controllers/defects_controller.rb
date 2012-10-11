414be2a808a697704670c36e5697d38098c6a797
Perry Smith <pedz@easesoftware.com>
2012-10-11 15:56:53 -0500

    Small update... need to implement a way to configure paths for CMVC

--- app/controllers/defects_controller.rb
+++ app/controllers/defects_controller.rb
@@ -1,4 +1,19 @@
 class DefectsController < ApplicationController
   def show
+    defect_name = params[:id]
+    @defect = Defect.find_by_name(defect_name)
+    if @defect.nil?
+      @defect = Defect.new(:name => defect_name)
+      lines = @defect.text
+      if lines.empty?
+        flash[:fatal] = "#{defect_name} not found"
+      else
+        @defect.save
+      end
+    end
+    
+    respond_to do |format|
+      format.html # show.html.erb
+    end
   end
 end
