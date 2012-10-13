63037e8852d26fd7ac7621556e869a804432d1c0
Perry Smith <pedz@easesoftware.com>
2012-10-12 21:03:45 -0500

    Start of cmvc config

---
+++ lib/cmvc_host.rb
@@ -0,0 +1,6 @@
+#
+# A class to help issue CMVC commands from a particular host.  It will
+# be configured by the cmvc.yml file in the config directory (which
+# will be loaded by an initializer).
+class CmvcHost
+end
