cdc82d7784f88f786852acfac096cb8596b16c8e
Perry Smith <pedz@easesoftware.com>
2012-10-14 11:15:14 -0500

    snapshop

--- lib/cmvc_host.rb
+++ lib/cmvc_host.rb
@@ -3,4 +3,5 @@
 # be configured by the cmvc.yml file in the config directory (which
 # will be loaded by an initializer).
 class CmvcHost
+  Config = YAML.load(File.open("#{RAILS_ROOT}/config/cmvc.yml"))
 end
