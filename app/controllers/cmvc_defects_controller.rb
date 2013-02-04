5016fdc4b8af140fb44a18c0209d72f1062130d6
Perry Smith <pedz@easesoftware.com>
2013-02-03 19:47:43 -0600

    git is acting weird

--- app/controllers/cmvc_defects_controller.rb
+++ app/controllers/cmvc_defects_controller.rb
@@ -5,20 +5,9 @@
 #
 
 class CmvcDefectsController < ApplicationController
+  respond_to :html, :json
+
   def show
-    defect_name = params[:defect]
-    return redirect_to defects_path(defect_name) if request.post?
-
-    # find or initialize the record and fetch the text.
-    @defect = Defect.find_or_initialize_by_name(defect_name)
-    @defect.fetch_text(user.cmvc)
-
-    # If this is a new record and fetch_text did not throw an
-    # exception, we know it is a valid defect number and can save it.
-    @defect.save if @defect.new_record?
-
-    respond_to do |format|
-      format.html # show.html.erb
-    end
+    respond_with(create_presenter(CmvcDefect.new(params)))
   end
 end
