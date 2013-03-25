52f3c46a983b76d3b9d3993cca0880fd95675ab5
Perry Smith <pedz@easesoftware.com>
2013-03-25 12:19:21 -0500

    Trying to deploy.  Need to note initialize jsroutes in production

--- config/initializers/jsroutes.rb
+++ config/initializers/jsroutes.rb
@@ -3,9 +3,11 @@
 #  Copyright 2012-2013 Ease Software, Inc. and Perry Smith
 #  All Rights Reserved
 # 
-JsRoutes.setup do |config|
-  config.namespace = 'condor3.routes'
-  if Rails.env == 'development'
-    config.prefix = '/condor3/'
+if Rails.env != 'production'
+  JsRoutes.setup do |config|
+    config.namespace = 'condor3.routes'
+    if Rails.env == 'development'
+      config.prefix = '/condor3/'
+    end
   end
 end
