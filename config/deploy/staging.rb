3c6feb6507f854b5f2b58ca6a078931f0f24d86e
Perry Smith <pedz@easesoftware.com>
2017-06-23 13:15:04 -0500

    Trying to make the Odroid work -- starting here

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server "condor3.austin.ibm.com", user: "condor", roles: %w{app db web}
 
 # role-based syntax
 # ==================
